78b5a69ff130c8b6c83643e859fa82651b67c43f
carlos andres orozco <carlosandresorozcoillera@gmail.com>
2024-10-12 11:14:09 -0500

    grabar y editar

--- _Repositories/PayModeRepository.cs
+++ _Repositories/PayModeRepository.cs
@@ -57,7 +57,20 @@ namespace Supermarket_mvp._Repositories
 
         public void Edit(PayModeModel payModeModel)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString)) 
+            using (var command = new SqlCommand()) 
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"UPDATE PayMode
+                                       SET Pay_Mode_Name =@name,
+                                       Pay_Mode_Observation = @observation
+                                       WHERE Pay_Mode_Id =@id";
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value=payModeModel.Name;
+                command.Parameters.Add("@observation", SqlDbType.NVarChar).Value= payModeModel.Observation;
+                command.Parameters.Add("@id", SqlDbType.Int).Value = payModeModel.Id;
+                command.ExecuteNonQuery();
+            }
         }
 
         public IEnumerable<PayModeModel> GetAll()
